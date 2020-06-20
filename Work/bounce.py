b5ca3c218806432c9922370445166a9fdf5772c4
Rifat Majumder <rifat.majumder@airtasker.com>
2020-06-20 23:52:07 +1000

    Add Exercise 1.5 solution

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # Meters
+bounce_count = 1
+
+while bounce_count <= 10:
+    height *=  0.6
+    print(bounce_count, round(height, 4))
+    bounce_count += 1
