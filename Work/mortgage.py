3afb69e669bb546be67427655fd3b3c1b2b7a2be
Rifat Majumder <rifat.majumder@airtasker.com>
2020-06-21 12:13:33 +1000

    Add Exercise 1.8 solution

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,25 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+extra_payment = 1000.0
+total_paid = 0.0
+month = 0
+
+while principal > 0:
+    month = month + 1
+
+    if month <= 12:
+        principal = principal * (1+rate/12) - (payment + extra_payment)
+        total_paid = total_paid + payment + extra_payment
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+
+
+
+print('Total paid', round(total_paid, 1))
+print('Months required', month)
